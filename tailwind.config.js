64d5f6764d00dfa30dba3f814ec3988880098190
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-19 17:16:58 -0600

    Added font-size to the config file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,12 @@ export default {
       sm: '0px 2px 4px rgb(11, 10, 55, 0.15)', 
       lg: '0px 8px 20px rgb(18, 16, 99, 0.06)',
     },
+    fontSize: {
+      headingM: ['32px', { lineHeight: '48px',}],
+      headingS: ['16px', { lineHeight: '24px',}],
+      bodyM: ['16px', { lineHeight: '24px',}],
+      bodyS: ['12px', { lineHeight: '18px',}],
+    }
     extend: {
       
     },
