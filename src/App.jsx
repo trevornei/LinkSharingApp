2736e9f6334ab2b437c347197c040f050413120c
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-19 11:46:03 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex flex-col'>
+      <h1 className="text-2xl font-bold underline">
+      Hello world!
+      </h1>
+      <h3 className="text-3xl">Welcome to my ----{">"} Link Sharing App</h3>
+      <p className="text-md">
+
+      </p>
+    </div>
+  )
+}
+
+export default App
