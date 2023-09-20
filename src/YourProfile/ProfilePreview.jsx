4c65b02bdd9d6b41e07ecde723e30549cd5686a2
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-20 08:47:31 -0600

    Added the href to nav button preview component inside landing page nav.

---
+++ src/YourProfile/ProfilePreview.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProfileNav from './ProfileNav/ProfileNav'
+
+export default function ProfilePreview() {
+  return (
+    <div className=''>
+        <ProfileNav/>
+    </div>
+  )
+}
--- src/components/nav/PreviewButton.jsx
+++ src/components/nav/PreviewButton.jsx
@@ -4,7 +4,11 @@ export default function PreviewButton() {
   return (
     <div className=''>
         <button className="border-2 border-purple rounded-xl">
-            <h3 className="p-2 mx-3">Preview</h3>
+            <h3 className="p-2 mx-3">
+                <a href="../../../YourProfile/ProfilePreview">
+                  Preview
+                </a>
+              </h3>
         </button>
     </div>
   )
