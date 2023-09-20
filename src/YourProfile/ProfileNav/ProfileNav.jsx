e39fca487eaa5ec718b871e21189cdd63dfbadfa
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-20 08:41:23 -0600

    Commit

--- src/YourProfile/ProfileNav/ProfileNav.jsx
+++ src/YourProfile/ProfileNav/ProfileNav.jsx
@@ -4,12 +4,12 @@ export default function ProfileNav() {
   return (
     <div className='flex flex-row items-center justify-between w-full h-auto'>
         <button className="border-2 border-purple purple flex items-center justify-center">
-            <h3 className="text-purple">
+            <h3 className="text-purple font-semibold">
                 Back to Editor
             </h3>
         </button>
         <button className="bg-purple p-2 flex items-center justify-center">
-            <h3 className="">
+            <h3 className="text-white font-semibold">
                 Share Link
             </h3>
         </button>
