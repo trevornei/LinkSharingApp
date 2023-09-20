c122087fe3716e7184aefbefbd0fe823b3a31677
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-19 18:01:42 -0600

    Added the components folder and added Nav component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
+import Nav from './components/nav'
 
 function App() {
   return (
-    <div className='flex flex-col bg-darkGrey rounded-xl'>
-      <h1 className="text-2xl font-bold text-white underline font-iSans">
-      Hello world!
-      </h1>
-      <h3 className="text-3xl text-purple">Welcome to my ----{">"} Link Sharing App</h3>
-      <p className="text-purpleH font-iSansI">
-        Testing Testing TestINNNNG!
-      </p>
-    </div>
+    <Nav/>
   )
 }
 
---
+++ src/components/nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <div className="">
+            <h1 className="text-purple font-iSans text-headingM font-bold">Test</h1>
+        </div>
+    )
+}
